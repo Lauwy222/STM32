7555a2c6a0ba5618bfd8c4ba25a4a8be3892642c
Laurens van der Drift <Lauwy222@hotmail.com>
2024-01-09 14:30:02 +0100

    working

--- Week13 XYZ/Core/Src/main.c
+++ Week13 XYZ/Core/Src/main.c
@@ -40,21 +40,16 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-I2C_HandleTypeDef hi2c1;
-
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-static const uint8_t TMP102_ADDR = 0x48;
-//static const uint8_t TMP102_ADDR = 0x72;
-static const uint8_t REG_TEMP = 0x00;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -71,10 +66,7 @@ static void MX_I2C1_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	HAL_StatusTypeDef ret;
-	uint8_t buf[12];
-	int16_t val;
-	float temp_c;
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -96,7 +88,6 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
-  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -105,29 +96,6 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  buf[0] = REG_TEMP;
-	  ret = HAL_I2C_Master_Transmit(&hi2c1, TMP102_ADDR << 1, buf, 1, HAL_MAX_DELAY);
-	  if(ret!=HAL_OK){
-		  strcpy((char*)buf, "TX ERRORED\r\n");
-	  } else {
-		  ret = HAL_I2C_Master_Receive(&hi2c1, TMP102_ADDR << 1, buf, 2, HAL_MAX_DELAY);
-		  if(ret!=HAL_OK){
-		  strcpy((char*)buf, "RX ERRORED\r\n");
-		  } else {
-
-			  val = ((int16_t)buf[0]<< 4) | (buf[1] >> 4);
-
-			  if (val > 0x7FF){
-				  val |= 0xF000;
-			  }
-			  temp_c = val*0.0625;
-			  temp_c *=100;
-			  sprintf((char*)buf,"%u.%02u C\r\n",((unsigned int)temp_c/100),((unsigned int)temp_c%100));
-		  }
-	  }
-//	  strcpy((char*)buf, "Hello!\r\n");
-	  HAL_UART_Transmit(&huart2, buf, strlen((char*)buf), HAL_MAX_DELAY);
-	  HAL_Delay(100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -180,40 +148,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
-
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
 }
 
 /**
